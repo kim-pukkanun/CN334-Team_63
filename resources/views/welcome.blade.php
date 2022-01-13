c5417e91a46967d2b28462b4b0f8f8e5fbe8943e
Ponlapat Ittiratchapong <webmaster@maxnus.com>
2022-01-13 14:49:22 +0700

    - Navbar with MDBootstrap 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                    Welcome
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Your Application's Landing Page.
+                    </p>
                 </div>
             </div>
         </div>
