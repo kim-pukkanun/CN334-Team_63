e4e3139ac2817509e9811fe8a3006eec2a423982
kim-pukkanun <pukkanun.som@dome.tu.ac.th>
2022-01-15 13:10:27 +0700

    Add Expired Task start at line 86 (Currently still use the same variable as task)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">
-                    Tasker
+            <div class="card" style="margin-top:30px">
+                <div class="card-header text-center">
+                    Welcome to Tasker
                 </div>
                 <div class="card-body">
-                    <p class="card-text">
-                        Welcome to Tasker.
+                    <p class="card-text text-center">
+                        Go finish your work and be done with it.
                     </p>
                 </div>
             </div>
