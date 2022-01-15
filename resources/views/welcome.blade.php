718a76dcc3c6451addd3b4d3a964853ca7aca976
kim-pukkanun <pukkanun.som@dome.tu.ac.th>
2022-01-15 12:39:11 +0700

    Change New Task and Current Task style

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                    Tasker
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Welcome to Tasker.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
